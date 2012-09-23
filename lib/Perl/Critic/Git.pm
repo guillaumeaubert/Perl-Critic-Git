2773f0133edc696f8350421c239a6617c9753dba
Guillaume Aubert <aubertg@cpan.org>
2012-09-22 23:59:55 -0400

    Released version 1.1.1.

--- lib/Perl/Critic/Git.pm
+++ lib/Perl/Critic/Git.pm
@@ -17,11 +17,11 @@ Perl::Critic::Git - Bond git and Perl::Critic to blame the right people for viol
 
 =head1 VERSION
 
-Version 1.1.0
+Version 1.1.1
 
 =cut
 
-our $VERSION = '1.1.0';
+our $VERSION = '1.1.1';
 
 
 =head1 SYNOPSIS
