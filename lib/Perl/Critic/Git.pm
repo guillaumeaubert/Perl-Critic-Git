17a17e74b8cffd4b28580bc0e5d1ea54e08acd57
Guillaume Aubert <aubertg@cpan.org>
2013-03-23 16:34:08 -0400

    Released version 1.1.4.

--- lib/Perl/Critic/Git.pm
+++ lib/Perl/Critic/Git.pm
@@ -17,11 +17,11 @@ Perl::Critic::Git - Bond git and Perl::Critic to blame the right people for viol
 
 =head1 VERSION
 
-Version 1.1.3
+Version 1.1.4
 
 =cut
 
-our $VERSION = '1.1.3';
+our $VERSION = '1.1.4';
 
 
 =head1 SYNOPSIS
